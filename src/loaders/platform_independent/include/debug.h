c358d82a5782a5768f3f00795e607d99c052d507
Rhys Rustad-Elliott <me@rhysre.net>
2020-11-22 01:08:58 -0800

    Beautify multiline macro backslashes

--- src/loaders/platform_independent/include/debug.h
+++ src/loaders/platform_independent/include/debug.h
@@ -13,71 +13,71 @@
 #endif
 
 #ifdef DEBUG_OUTPUT
-#define DEBUG_FMT(fmtstr, ...) \
+#define DEBUG_FMT(fmtstr, ...)                                                \
   minimal_printf(1, KITESHIELD_PREFIX fmtstr "\n", __VA_ARGS__)
 #else
 #define DEBUG_FMT(fmtstr, ...) do {} while (0)
 #endif
 
 #ifdef DEBUG_OUTPUT
-#define DIE(msg) \
-  do { \
-    minimal_printf(2, KITESHIELD_PREFIX msg "\n"); \
-    exit(1); \
+#define DIE(msg)                                                              \
+  do {                                                                        \
+    minimal_printf(2, KITESHIELD_PREFIX msg "\n");                            \
+    exit(1);                                                                  \
   } while (0)
 #else
-#define DIE(msg) \
-  do { \
-    if (cond) { \
-      exit(0); \
-    } \
+#define DIE(msg)                                                              \
+  do {                                                                        \
+    if (cond) {                                                               \
+      exit(0);                                                                \
+    }                                                                         \
   } while (0)
 #endif
 
 #ifdef DEBUG_OUTPUT
-#define DIE_FMT(msg, ...) \
-  do { \
-    minimal_printf(2, KITESHIELD_PREFIX msg "\n", __VA_ARGS__); \
-    exit(1); \
+#define DIE_FMT(msg, ...)                                                     \
+  do {                                                                        \
+    minimal_printf(2, KITESHIELD_PREFIX msg "\n", __VA_ARGS__);               \
+    exit(1);                                                                  \
   } while (0)
 #else
-#define DIE_FMT(msg, ...) \
-  do { \
-    exit(0); \
+#define DIE_FMT(msg, ...)                                                     \
+  do {                                                                        \
+    exit(0);                                                                  \
   } while (0)
 #endif
 
 #ifdef DEBUG_OUTPUT
-#define DIE_IF(cond, msg) \
-  do { \
-    if (cond) { \
-      minimal_printf(2, KITESHIELD_PREFIX msg "\n"); \
-      exit(1); \
-    } \
+#define DIE_IF(cond, msg)                                                     \
+  do {                                                                        \
+    if (cond) {                                                               \
+      minimal_printf(2, KITESHIELD_PREFIX msg "\n");                          \
+      exit(1);                                                                \
+    }                                                                         \
   } while (0)
 #else
-#define DIE_IF(cond, msg) \
-  do { \
-    if (cond) { \
-      exit(0); \
-    } \
+#define DIE_IF(cond, msg)                                                     \
+  do {                                                                        \
+    if (cond) {                                                               \
+      exit(0);                                                                \
+    }                                                                         \
   } while (0)
 #endif
 
 #ifdef DEBUG_OUTPUT
-#define DIE_IF_FMT(cond, msg, ...) \
-  do { \
-    if (cond) { \
-      minimal_printf(2, KITESHIELD_PREFIX msg "\n", __VA_ARGS__); \
-      exit(1); \
-    } \
+#define DIE_IF_FMT(cond, msg, ...)                                            \
+  do {                                                                        \
+    if (cond) {                                                               \
+      minimal_printf(2, KITESHIELD_PREFIX msg "\n", __VA_ARGS__);             \
+      exit(1);                                                                \
+    }                                                                         \
   } while (0)
 #else
-#define DIE_IF_FMT(cond, msg, ...) \
-  do { \
-    if (cond) { \
-      exit(0); \
-    } \
+#define DIE_IF_FMT(cond, msg, ...)                                            \
+  do {                                                                        \
+    if (cond) {                                                               \
+      exit(0);                                                                \
+    }                                                                         \
   } while (0)
 #endif
 

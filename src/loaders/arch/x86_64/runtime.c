f82a187462ec08082c2a783e07449f4da131e6d9
Rhys Rustad-Elliott <me@rhysre.net>
2020-11-23 20:26:35 -0800

    Add beginnings of runtime decryption
    - Add runtime.c, which contains code that ptraces and dynamically
    decrypts functions in the main thread
    - Add asm to call into runtime.c in a subprocess

---
+++ src/loaders/arch/x86_64/runtime.c
@@ -0,0 +1,8 @@
+#include "loaders/arch/x86_64/include/syscall_defines.h"
+#include "loaders/platform_independent/include/arch_typedefs.h"
+#include "loaders/platform_independent/include/debug.h"
+#include "loaders/platform_independent/include/syscalls.h"
+
+void runtime_start() {
+  return;
+}

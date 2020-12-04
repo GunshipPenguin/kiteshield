85f3a6115f0652d7be7dd91565992421ebee9f14
Rhys Rustad-Elliott <me@rhysre.net>
2020-12-03 22:19:25 -0800

    Rename arch_typedefs.h to types.h

--- loader/include/types.h
+++ loader/include/types.h
@@ -3,12 +3,8 @@
 
 #define NULL 0
 
-#ifdef __amd64__
 typedef unsigned long long size_t;
 typedef signed long long ssize_t;
 typedef unsigned long long off_t;
-#else
-#error "Architechure not supported"
-#endif
 
 #endif

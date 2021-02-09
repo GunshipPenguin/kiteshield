a6a067a195326eec826aaa19f79807ab3471a253
Rhys Rustad-Elliott <me@rhysre.net>
2021-02-08 20:40:10 -0800

    Add more docs to anti_debug.c

--- loader/anti_debug.c
+++ loader/anti_debug.c
@@ -1,3 +1,5 @@
+/* Note most actual antidebug code is defined in this header file as it
+ * has to be inlined everywhere. */
 #include "loader/include/anti_debug.h"
 
 int sigtrap_counter = 0;
@@ -21,6 +23,15 @@ void antidebug_signal_init()
   DIE_IF_FMT(res < 0, "rt_sigaction failed with errro %d", res);
 }
 
+/* Sets the process's dumpable flag to 0. This makes ptrace attaches impossible
+ * and disables coredumping. We can only do this in the parent of course, since
+ * the child needs to be ptraced by the process running the runtime.
+ *
+ * While the latter point above (disabling coredumping) is also achieved
+ * through our use of rlimit to set the max core dump size to 0, this is still
+ * one more thing a reverse engineer has to get around, which makes it a
+ * positive.
+ */
 void antidebug_prctl_set_nondumpable()
 {
 #ifdef NO_ANTIDEBUG
